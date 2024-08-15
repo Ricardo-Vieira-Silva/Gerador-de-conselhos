ed2c303bd4e95cc849930e306e441a504cac0625
Ricardo Vieira <ricardovieira7458@gmail.com>
2024-08-15 20:11:19 -0300

    Correcoes

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const botaoConselho = document.getElementById('conselho');
 const tituloConselho = document.querySelector('.titulo');
 const textoConselho = document.querySelector('.texto');
@@ -17,24 +16,4 @@ async function buscarConselho() {
 }
 
 
-=======
-const botaoConselho = document.getElementById('conselho');
-const tituloConselho = document.querySelector('.titulo');
-const textoConselho = document.querySelector('.texto');
-
-async function buscarConselho() {
-    try {
-        const resposta = await fetch('https://api.adviceslip.com/advice');
-        const dados = await resposta.json();
-
-        tituloConselho.innetText = `ADVICE #${dados.slip.id}`;
-        textoConselho.innerText = `"${dados.slip.advice}"`;
-
-    } catch (erro) {
-        console.error('Erro ao buscar o conselho:', erro);
-    }
-}
-
-
->>>>>>> d42156889aac71b061060ad05b65880e657193ac
 botaoConselho.addEventListener('click', buscarConselho);
